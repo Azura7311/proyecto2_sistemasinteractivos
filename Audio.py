e5b525102a57a4f8d6e4beee9649feec74fcbde2
Azura7311 <azura7311@gmail.com>
2024-09-06 01:49:09 -0500

    Make First ending

--- Audio.py
+++ Audio.py
@@ -1,15 +1,16 @@
 from openal import *
 class Audio:
-    def __init__(self, path, volume = 0.5, position = [0, 0, 0], velocity = [0, 0, 0]):
+    def __init__(self, path, volume = 0.25, position = [0, 0, 0], velocity = [0, 0, 0]):
         self.path = path
         self.position = position
         self.velocity = velocity
         self.volume = volume
         self.source = oalOpen(self.path)
-        self.source.set_source_relative(True)
         self.source.set_position(self.position)
-        self.source.set_velocity(self.velocity)
+        #self.source.set_velocity(self.velocity)
         self.source.set_gain(self.volume)
+        listener = Listener()
+        listener.set_position([0, 0, 0])
     def play(self):
         self.source.play()
         print(self.source.position)
--- Controller.py
+++ Controller.py
@@ -1,5 +1,7 @@
 from Section import *
-
+from openal import *
+import time
+from random import choice
 class Controller:
     def __init__(self, sections):
         self.sections = sections
@@ -7,7 +9,13 @@ class Controller:
         self.ambient.play()
     def startGame(self):
         next = 0
-        while(next != -1):
+        while(next > -1):
+            if next == 8:
+                ans = self.minigame()
+                if ans == -1:
+                    next = 9
+                else:
+                    next = 10
             actual = self.sections[next]
             if actual.ambient:
                 self.ambient.stop()
@@ -16,4 +24,23 @@ class Controller:
             if actual.sfx:
                 actual.sfx.play()
             next = self.sections[next].play()
-        oalQuit()
+    def minigame(self):
+        result = 0
+        print("Presta atención a la dirección de los pasos. Dispara a la izquierda (0) o derecha(1). Afortunadamente, no te pueden ver tras los arbustos. Preparaté...")
+        time.sleep(5)
+        print("3...")
+        print("2...")
+        print("1...")
+        print("YA.")
+        sfx = oalOpen("./sfx/running_leaves.wav")
+        for i in range(5):
+            dir = choice([-1, 1])
+            sfx.set_position([5 * dir, 0, 0])
+            sfx.play()
+            ans = input("0/1: ")
+            sfx.stop()
+            if ans == 0 and dir == 1 or ans == 1 and dir == -1:
+                return -1
+        return 0
+            
+    #oalQuit()
